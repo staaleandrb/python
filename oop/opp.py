61748643f295036f4b65bde5aa272b66719a5a0f
staaleandrb <staale@ingsta.com>
2024-04-16 09:22:00 +0200

    test oop

---
+++ oop/opp.py
@@ -0,0 +1,8 @@
+class Vehicle:
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+    def seating_capacity(self, capacity):
+        return f"The seating capacity of a {self.name} is {capacity} passengers"
