6e04f56eec2b9a75283b377a82f9cb316556a362
staaleandrb <staale@ingsta.com>
2024-05-26 10:54:43 +0200

    Telle antall av den enkelte bokstav

--- kryptering/caesarsChiffer/caesarsChiffer.py
+++ kryptering/caesarsChiffer/caesarsChiffer.py
@@ -1,20 +1,18 @@
-
-For å lese gjennom filen og telle bokstaver med kun én løkke, kan du bruke en itererbar måte å lese filen direkte i en løkke. Her er en måte å gjøre det på:
-
-python
-Kopier kode
 alfa = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
         "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 antall = [0] * len(alfa)  # Initialiserer telleren til null for hver bokstav
 
 def read_file(filnavn):  # Åpner fila og leser innholdet av fila
     f = open(filnavn, "r")  # Bruker 'with' for å sikre at filen lukkes korrekt
-    for char in f  # Leser hele filen og itererer gjennom hvert tegn
-        print(char)
-        #if char in alfa:
-        #    antall[alfa.index(char)] += 1  # Øker telleren for riktig bokstav
+    for tekst in f:  # Leser hele filen og itererer gjennom hvert tegn
+        for bokstav in tekst:
+            if bokstav in alfa:
+                antall[alfa.index(bokstav)] += 1
+    skriut()
     
-    print(antall)
+def skriut():
+    for i in range(len(alfa)):
+        print(alfa[i], antall[i])
+    
 
-# Eksempel på bruk
 read_file("kryptering/caesarsChiffer/kryptert.txt")
