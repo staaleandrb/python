388f53393861295c138447119d8c92bbb704a791
staaleandrb <staale@ingsta.com>
2024-05-26 10:37:24 +0200

    endringer

--- kryptering/caesarsChiffer/caesarsChiffer.py
+++ kryptering/caesarsChiffer/caesarsChiffer.py
@@ -1,12 +1,19 @@
-alfa = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+For å lese gjennom filen og telle bokstaver med kun én løkke, kan du bruke en itererbar måte å lese filen direkte i en løkke. Her er en måte å gjøre det på:
+
+python
+Kopier kode
+alfa = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 antall = [0] * len(alfa)  # Initialiserer telleren til null for hver bokstav
 
 def read_file(filnavn):  # Åpner fila og leser innholdet av fila
-    with open(filnavn, "r") as f:  # Bruker 'with' for å sikre at filen lukkes korrekt
-        for line in f:  
-            for char in line.lower():  # Konverterer til små bokstaver
-                if char in alfa:
-                    antall[alfa.index(char)] += 1  # Øker telleren for riktig bokstav
+    f = open(filnavn, "r")  # Bruker 'with' for å sikre at filen lukkes korrekt
+    for char in f  # Leser hele filen og itererer gjennom hvert tegn
+        print(char)
+        #if char in alfa:
+        #    antall[alfa.index(char)] += 1  # Øker telleren for riktig bokstav
+    
     print(antall)
 
 # Eksempel på bruk
